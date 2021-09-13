129302b95901138f2d7e6a1cfb8baa4cbbaacb4b
Jalilov <javohirjalilov777@gmail.com>
2021-09-13 09:46:25 +0500

    upload image database

--- django_course_site/urls.py
+++ django_course_site/urls.py
@@ -20,4 +20,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('meetups.urls'))
-] + static(settings.MEDIA_URL)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
